6aac88c4735a863e1a3136e4d9aec52553a213d6
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2022-07-17 13:49:19 +0200

    Add simple validation script

--- README.md
+++ README.md
@@ -13,25 +13,25 @@ A list of origami related resources.
 
 **[`^        back to top        ^`](#)**
 
-- [Oriedita](https://oriedita.github.io) Crease Pattern Editor based on Orihime
-- [Oripa](https://github.com/oripa/oripa) Crease Pattern Editor
-- [Orihime](http://mt777.html.xdomain.jp/) Crease Pattern Editor
-- [Oripa (Android)](https://play.google.com/store/apps/details?id=com.origamitoolbox.oripa&gl=US) Crease Pattern Editor
-- [OrigamiDraw (iOS)](https://apps.apple.com/us/app/origamidraw/id1268158815) Crease Pattern Editor
+- [Oriedita](https://oriedita.github.io) - Crease Pattern Editor based on Orihime
+- [Oripa](https://github.com/oripa/oripa) - Crease Pattern Editor ([Demoss](http://google.com))
+- [Orihime](http://mt777.html.xdomain.jp/) - Crease Pattern Editor
+- [Oripa (Android)](https://play.google.com/store/apps/details?id=com.origamitoolbox.oripa&gl=US) - Crease Pattern Editor for Android
+- [OrigamiDraw (iOS)](https://apps.apple.com/us/app/origamidraw/id1268158815) - Crease Pattern Editor for iOS
 
 ## Community
 
 **[`^        back to top        ^`](#)**
 
-- [Origami-dan (折り紙団)](https://disboard.org/server/553401275127955476) Origami Discord server
-- [snkhan.co.uk](https://snkhan.co.uk/forum/index.php) Origami forums
+- [Origami-dan (折り紙団)](https://disboard.org/server/553401275127955476) - Origami Discord server
+- [snkhan.co.uk](https://snkhan.co.uk/forum/index.php) - Origami forums
 
 ## Theory
 
 **[`^        back to top        ^`](#)**
 
-- [Abrashi Origami School](https://abrashiorigami.com/)
-- [Tsai Origami](https://origami.abstreamace.com/)
+- [Abrashi Origami School](https://abrashiorigami.com/) - Blog about origami design theory
+- [Tsai Origami](https://origami.abstreamace.com/) - Blog about origami theory
 
 ## Techniques
 
---
+++ tests/validate.js
@@ -0,0 +1,193 @@
+// USAGE:
+// node validate.js -r README.md  (Checks whole file)
+// node validate.js -r README.md -d temp.md  (Checks just the diff)
+
+import fs from "fs";
+import chalk from "chalk";
+
+const LCERROR = '\x1b[31m%s\x1b[0m'; //red
+const LCWARN = '\x1b[33m%s\x1b[0m'; //yellow
+const LCINFO = '\x1b[36m%s\x1b[0m'; //cyan
+const LCSUCCESS = '\x1b[32m%s\x1b[0m'; //green
+
+const logger = class {
+    static error(message, ...optionalParams) { console.error(LCERROR, message, ...optionalParams) }
+    static warn(message, ...optionalParams) { console.warn(LCWARN, message, ...optionalParams) }
+    static info(message, ...optionalParams) { console.info(LCINFO, message, ...optionalParams) }
+    static success(message, ...optionalParams) { console.info(LCSUCCESS, message, ...optionalParams) }
+}
+
+const ALLOWED_LINK_TYPES = ['Demo', 'Source Code']
+
+let pr = false;
+let readme;
+let diff;
+
+// Detect if we find an entry: - [asdf](http://asdf) - Description
+const ENTRY_DETECT = /^\s{0,2}[-*]\s\[.*\).*-/
+const ENTRY_SELECT = /^\s{0,2}-\s(?<link>\[[^\]]+\]\([^)]+\))\s*(?<tag>`.+`)?\s*-\s+(?<description>[^)]{0,250}?)\s?(?<links>\(.+\))?$/
+const LINK_SELECT = /\[(?<name>[^\]]+)]\((?<url>[^)]+)\)/g;
+
+//Parse the command options and set the pr var
+function parseArgs(args) {
+    if (args.indexOf('-r', 2) > 0) {
+        readme = fs.readFileSync(args[args.indexOf('-r', 2) + 1], 'utf8')
+    }
+    if (args.indexOf('-d', 2) > 0) {
+        pr = true;
+        diff = fs.readFileSync(args[args.indexOf('-d', 2) + 1], 'utf8');
+    }
+    if (pr === true) {
+        logger.info(`Running on PR. README.md: ${args[args.indexOf('-r', 2) + 1]} diff: ${args[args.indexOf('-d', 2) + 1]}`)
+    }
+}
+
+// Function to find lines with entries
+function entryFilter(md) {
+    return ENTRY_DETECT.test(md);
+}
+
+// Function to split lines into array
+function split(text) {
+    return text.split(/\r?\n/);
+}
+
+//Test '- [Name](http://homepage/)'
+function testMainLink(text) {
+    let link = text.split('-')[1].trim();
+
+    if (!/^\[[^\]]+]\([^)]+\)$/.test(link)) {
+        logger.error(text + " Link part is not formatted like [...](...)")
+        return false;
+    }
+
+    return true;
+}
+
+//Test  '`⚠` - Short description, less than 250 characters.'
+function testDescription(text) {
+    const descPart = text.split('-')[2].trim()
+
+    if (descPart.length > 250) {
+        logger.error("Description too long");
+        return false;
+    }
+
+    return true;
+}
+
+//If present, tests '([Demo](http://url.to/demo), [Source Code](http://url.of/source/code), [Clients](https://url.to/list/of/related/clients-or-apps))'
+function testLinks(line, entry) {
+
+    let links = entry.links
+    if (links) {
+        let parsedLinks = [...links.matchAll(LINK_SELECT)]
+        for (let i = 0; i < parsedLinks.length; i++) {
+            if (!ALLOWED_LINK_TYPES.includes(parsedLinks[i].groups.name)) {
+                console.error(chalk.red(`${line}: Link name '${parsedLinks[i].groups.name}' not in ${ALLOWED_LINK_TYPES.join(", ")}`))
+
+                return false;
+            }
+        }
+    }
+
+
+    return true;
+}
+
+//Runs all the syntax tests...
+function findError(text) {
+    testMainLink(text);
+    testDescription(text);
+}
+
+const checkEntry = (e) => {
+    let entry = ENTRY_SELECT.exec(e.raw)
+
+    if (!entry) {
+        logger.error(`Errors for line ${e.line}:`)
+        findError(e.raw);
+
+        return false;
+    }
+
+    if (!testLinks(e.line, entry.groups)) {
+        return false;
+    }
+
+    return true;
+}
+
+function entryErrorCheck() {
+    const lines = split(readme); // Inserts each line into the entries array
+    let totalFail = 0;
+    let totalPass = 0;
+    let total = 0;
+    let entries = [];
+    let diffEntries = [];
+
+    if (lines[0] === "") {
+        console.log(chalk.red("0 Entries Found, check your commandline arguments"))
+        process.exit(0)
+    }
+    for (let i = 0; i < lines.length; i++) { // Loop through array of lines
+        if (entryFilter(lines[i]) === true) { // filter out lines that don't start with * [)
+            let e = {};
+            e.raw = lines[i];
+            e.line = i + 1
+            entries.push(e);
+        }
+    }
+
+    if (pr === true) {
+        console.log(chalk.cyan("Only testing the diff from the PR.\n"))
+        const diffLines = split(diff); // Inserts each line of diff into an array
+        for (let l of diffLines) {
+            if (entryFilter(l) === true) { // filter out lines that don't start with * [)
+                let e = {};
+                e.raw = l;
+                diffEntries.push(e);
+            }
+        }
+        if (diffEntries.length === 0) {
+            console.log("No entries changed in README.md, Exiting...")
+            process.exit(0)
+        }
+        total = diffEntries.length
+        for (let e of diffEntries) {
+            let pass = checkEntry(e);
+
+            if (pass) {
+                totalPass++
+            } else {
+                totalFail++
+            }
+        }
+    } else {
+        console.log(chalk.cyan("Testing entire README.md\n"))
+        total = entries.length
+        for (let e of entries) {
+            let pass = checkEntry(e);
+
+            if (pass) {
+                totalPass++
+            } else {
+                totalFail++
+            }
+        }
+    }
+    if (totalFail > 0) {
+        console.log(chalk.blue(`\n-----------------------------\n`))
+        console.log(chalk.red(`${totalFail} Failed, `) + chalk.green(`${totalPass} Passed, `) + chalk.blue(`of ${total}`))
+        console.log(chalk.blue(`\n-----------------------------\n`))
+        process.exit(1);
+    } else {
+        console.log(chalk.blue(`\n-----------------------------\n`))
+        console.log(chalk.green(`${totalPass} Passed of ${total}`))
+        console.log(chalk.blue(`\n-----------------------------\n`))
+        process.exit(0)
+    }
+}
+
+parseArgs(process.argv)
+entryErrorCheck();
